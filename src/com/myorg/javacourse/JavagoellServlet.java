b02061454d6c2c505067d8becce4d962f8bab7b0
yuvalgoell <goellyuval@gmail.com>
2014-12-23 17:09:28 +0200

    Exercise 02-Math

--- src/com/myorg/javacourse/JavagoellServlet.java
+++ src/com/myorg/javacourse/JavagoellServlet.java
@@ -5,7 +5,12 @@ import javax.servlet.http.*;
 @SuppressWarnings("serial")
 public class JavagoellServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		int num1=4;
+		int num2=3;
+		int num3=7;
+		int result=(num1+num2)*num3;
+		resp.setContentType("text/html");
+		String resultStr= new String ("<h1>Result of ("+num1+"+"+num2+")*"+num3+"="+result+"</h1>");
+		resp.getWriter().println(resultStr);
 	}
 }
