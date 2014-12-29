35a77c9534ed22c182ffb5b8a5b1fc6230de2762
yuvalgoell <goellyuval@gmail.com>
2014-12-29 22:35:27 +0200

    exercie 3

--- src/com/myorg/javacourse/MyNewServlet.java
+++ src/com/myorg/javacourse/MyNewServlet.java
@@ -11,9 +11,37 @@ public class MyNewServlet extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 	throws ServletException, IOException {
-		
 		resp.setContentType("text/html");
-		resp.getWriter().println("HEllo my new Servlet");
+		
+		int radius=50;
+		double area= Math.PI*Math.pow(radius, 2);
+		int angleB=30;
+		int hypotenuse=50;
+		double opposite= Math.sin(Math.toRadians(angleB))*hypotenuse;
+        double power= Math.pow(20, 13);
+        
+        String line1= new String ("<b>Area of circle with radius</b> " +radius+ " <b>is</b>" +area+ " <b>square-cm</b>");
+		String line2= new String ("<b>Length of oppsite where angle B is 30 degrees and Hypotenuse length is 50 cm is:</b>" +opposite+ " <b>cm</b>");
+		String line3= new String ("<b>Power of 20 with exp of 13 is:</b>" +power++);
+		
+		String resultStr= line1 + "<br>" + line2 + "<br>" +line3;
+		resp.getWriter().println(resultStr);
+		
+	
+		
+		
+		
+		
+	
+		
+		 
+		
+		
+		
+				
+		
+		
+		
 	}
 
 }
