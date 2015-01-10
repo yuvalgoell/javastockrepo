beab3807590d90f4a8461a01bcbaba43675139f0
yuvalgoell <goellyuval@gmail.com>
2015-01-10 19:50:11 +0200

    exrecise 4

--- src/com/myorg/javacourse/MyNewServlet.java
+++ src/com/myorg/javacourse/MyNewServlet.java
@@ -20,9 +20,9 @@ public class MyNewServlet extends HttpServlet {
 		double opposite= Math.sin(Math.toRadians(angleB))*hypotenuse;
         double power= Math.pow(20, 13);
         
-        String line1= new String ("<b>Area of circle with radius</b> " +radius+ " <b>is</b>" +area+ " <b>square-cm</b>");
+        String line1= new String ("<b>Area of circle with radius</b> " + "<H1 style=color:green>"+radius+"</H1>" +  " <b>is</b>" +area+ " <b>square-cm</b>");
 		String line2= new String ("<b>Length of oppsite where angle B is 30 degrees and Hypotenuse length is 50 cm is:</b>" +opposite+ " <b>cm</b>");
-		String line3= new String ("<b>Power of 20 with exp of 13 is:</b>" +power++);
+		String line3= new String ("<b>Power of 20 with exp of 13 is:</b>" + power);
 		
 		String resultStr= line1 + "<br>" + line2 + "<br>" +line3;
 		resp.getWriter().println(resultStr);
